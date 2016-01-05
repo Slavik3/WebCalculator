2a8d7bdf1e6d5520ef77a9f44a1357f81f8a566a
Slavik3 <slavikkozlov3@gmail.com>
2016-01-05 22:16:02 +0200

    Update Calculator.java
    edit comment

--- src/ua/kharkov/knure/chapaev/Task2/Calculator.java
+++ src/ua/kharkov/knure/chapaev/Task2/Calculator.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Calculator
+ * test
  */
 public class Calculator extends HttpServlet {
 	private static final long serialVersionUID = 1L;
